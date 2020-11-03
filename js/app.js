8da743a0f4186dcdd600e1948df8c10c7f86c140
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-11-03 21:07:14 +0200

    Add: player movements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+const defaultPlayerState = {
+  x: 200,
+  y: 400,
+  speed: 25,
+}
+
+const playingArea = {
+  minX: 0, 
+  minY: -10,
+  maxX: 400,
+  maxY: 400,
+}
+
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function({x, y, speed}) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -27,10 +40,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function () {
+const Player = function ({x,y,speed}) {
   this.sprite = 'images/char-boy.png';
-  this.x = 200;
-  this.y = 400;
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
 };
 
 Player.prototype.update = function() {
@@ -41,19 +55,45 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-  
+Player.prototype.handleInput = function(key) {
+  switch (key) {
+    case 'left':
+      this.x -= this.speed;
+      if (this.x < playingArea.minX) {
+        this.x = playingArea.minX + this.speed;
+      }
+      break;
+    case 'right':
+      this.x += this.speed;
+      if (this.x > playingArea.maxX) {
+        this.x = playingArea.maxX - this.speed;
+      }
+      break;
+    case 'up':
+      this.y -= this.speed;
+      if (this.y <= playingArea.minY) {
+        alert('You won!');
+        this.x = defaultPlayerState.x;
+        this.y = defaultPlayerState.y;
+      }
+      break;
+    case 'down':
+      this.y += this.speed;
+      if (this.y > playingArea.maxY) {
+        this.y = playingArea.maxY - this.speed;
+      }
+      break;
+    default:
+  }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [];
 // Place the player object in a variable called player
-const player = new Player();
-
+const player = new Player(defaultPlayerState);
 
 // This listens for key presses and sends the keys to your
-const allEnemies = [];
-
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
